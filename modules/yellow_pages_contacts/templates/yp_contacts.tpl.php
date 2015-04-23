1d8252ce3aacc06307fc34162e00f75cdf6f06c6
amd_k8 <am@inlead.dk>
2015-04-24 01:57:43 +0300

    YP 12 - Contacts module.

---
+++ modules/yellow_pages_contacts/templates/yp_contacts.tpl.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file
+ * Template file for contacts.
+ */
+?>
+<div class="yp-contacts">
+  <div class="entry contact-mail">
+    <i class="fa fa-envelope-o"></i>
+    <span class="item"><?php print $mail; ?></span>
+  </div>
+  <div class="entry contact-phone">
+    <i class="fa fa-phone"></i>
+    <span class="item"><?php print $phone; ?></span>
+  </div>
+  <div class="entry contact-social">
+    <i class="fa fa-facebook"></i>
+    <span class="item"><?php print $url; ?></span>
+  </div>
+</div>

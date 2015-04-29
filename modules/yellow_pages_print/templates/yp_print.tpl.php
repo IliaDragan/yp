9192aa3185d17b29d2cfe31759fa59212d27c9a8
amd_k8 <am@inlead.dk>
2015-04-29 23:20:40 +0300

    YP 4 - Print button module.

---
+++ modules/yellow_pages_print/templates/yp_print.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * YellowPages rates block template.
+ */
+?>
+<div class="yp-print">
+  <a href="javascript:window.print();">
+    <span class="glyphicon glyphicon-print"></span><?php print t('Print'); ?>
+  </a>
+</div>

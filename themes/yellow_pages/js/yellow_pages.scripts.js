eb4fde30a687266d804e15b38654d59ca99afb5e
amd_k8 <am@inlead.dk>
2015-05-18 21:51:40 +0300

    Removed styles from theme.

--- themes/yellow_pages/js/yellow_pages.scripts.js
+++ themes/yellow_pages/js/yellow_pages.scripts.js
@@ -23,37 +23,12 @@ var Drupal = Drupal || {};
         mouseDrag : false,
         items: 4
       });
-
-      var $div;
-      var fields = ['.views-field-title', '.views-field-changed', '.views-field-body', '.views-field-nid'];
-      $('.view-id-news.view-display-id-default .view-content .owl-item').each(function (i, e) {
-        $div = $('<div />').addClass('views-row-field-wrapper');
-        $div.append('<div class="close"><i class="fa fa-times"></i></div>');
-        $(e).find('.views-row .views-field-field-list-image').after($div);
-        for (i in fields) {
-          $(e).find('.views-row ' + fields[i]).appendTo($div);
-        }
-      });
-
-      var $ele;
-      $('.views-row-field-wrapper').on('click', function (evt, e) {
-        $ele = $(this);
-
-        $ele.addClass('active');
-      });
-
-      $('.views-row-field-wrapper .close').on('click', function (evt, e) {
-        $ele = $(this).parents('.views-row-field-wrapper');
-
-        $ele.removeClass('active');
-        evt.stopPropagation();
-      });
     }
   }
 
   Drupal.behaviors.search_form = {
     attach : function () {
-      $('form.search-form input.form-text').attr('placeholder', Drupal.t('Facility search'));
+      $('.search-block-form input.form-text').attr('placeholder', Drupal.t('Facility search'));
     }
   }
 

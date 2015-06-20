030a765681411608b439fa0c61a7b86db9575b99
Занет Наталья <zanet.n@fruitware.ru>
2015-06-20 19:03:50 +0300

    Node styles for main page

--- themes/yellow_pages/templates/node/node--news--teaser.tpl.php
+++ themes/yellow_pages/templates/node/node--news--teaser.tpl.php
@@ -80,7 +80,7 @@
  * @ingroup themeable
  */
 ?>
-<div id="node-<?php print $node->nid; ?>" class="node-main-carousel <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <div class="content"<?php print $content_attributes; ?>>
     <div class="left">
       <?php print render($content['field_list_image']);?>

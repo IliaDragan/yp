500bf14d4d57dc287b37e33f98862cf82cea1d83
amdk8 <am@inlead.dk>
2015-06-26 12:43:10 +0300

    YP 36 - Fetch today currencies.

--- modules/yellow_pages_rates/templates/yp_rates.tpl.php
+++ modules/yellow_pages_rates/templates/yp_rates.tpl.php
@@ -6,25 +6,12 @@
 ?>
 <div class="yp-widget yp-rates">
   <h2 class="title">Currency rates <span class="info">(Banca Nationala)</span></h2>
+  <?php foreach($rates as $currency => $rate) : ?>
   <div class="entry">
-    <span class="usd currency info">USD</span>
-    <span class="amount">1</span>
-    <span class="rate">16.8675</span>
+    <span class="<?php print drupal_html_class($currency); ?> currency info"><?php print $currency; ?></span>
+    <span class="amount"><?php print $rate['nominal']; ?></span>
+    <span class="rate"><?php print $rate['value']; ?></span>
   </div>
-  <div class="entry">
-    <span class="eur currency info">EUR</span>
-    <span class="amount">1</span>
-    <span class="rate">19.5106</span>
-  </div>
-  <div class="entry">
-    <span class="ron currency info">RON</span>
-    <span class="amount">1</span>
-    <span class="rate">4.3331</span>
-  </div>
-  <div class="entry">
-    <span class="rub currency info">RUB</span>
-    <span class="amount">1</span>
-    <span class="rate">0.2583</span>
-  </div>
-  <p class="link"><a href="#">www.bnm.md</a></p>
+  <?php endforeach; ?>
+  <p class="link"><?php print l('http://bnm.md/', 'http://bnm.md/', array('attributes' => array('target' => '_blank'))); ?></p>
 </div>

63f64a6f842abc2e0f41087411c5e5d3beb9bb6e
amd_k8 <am@inlead.dk>
2015-08-05 18:14:30 +0300

    Topbar banner style.

--- themes/yellow_pages/templates/node/node--advertisement.tpl.php
+++ themes/yellow_pages/templates/node/node--advertisement.tpl.php
@@ -3,8 +3,12 @@
  * @file
  * Advertisement node template override.
  */
-
+// @todo
+// That's lame.
+// Move outside of template.
+$ad_image = file_create_url($content['field_ad_image']['#items'][0]['uri']);
 $content['field_ad_url']['#items'][0]['html'] = TRUE;
+$content['field_ad_url']['#items'][0]['attributes']['style'] = 'width:100%; height:90px; background: url("' . $ad_image . '") 50% 0 no-repeat; display:block;margin: 0 auto;border:none;';
 $type = field_get_items('node', $node, 'field_ad_type');
 $type = $type[0]['value'];
 $ad_url = isset($content['field_ad_url']['#items'][0]['url']) ? $content['field_ad_url']['#items'][0]['url'] : '';
@@ -14,7 +18,7 @@ $ad_body = isset($body[0]['safe_value']) ? $body[0]['safe_value'] : '';
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <div class="advertisement">
     <?php if ($type == 'image') : ?>
-    <?php print l(render($content['field_ad_image']), $ad_url, $content['field_ad_url']['#items'][0]); ?>
+    <?php print l('', $ad_url, $content['field_ad_url']['#items'][0]); ?>
     <?php else: ?>
     <?php print $ad_body; ?>
     <?php endif; ?>

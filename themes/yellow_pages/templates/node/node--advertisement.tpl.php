88ac7a02eb9250ac7325aa3b6983f099a44ba19e
amdk8 <am@inlead.dk>
2015-08-05 22:31:03 +0300

    Changes advertisement template logic.

--- themes/yellow_pages/templates/node/node--advertisement.tpl.php
+++ themes/yellow_pages/templates/node/node--advertisement.tpl.php
@@ -6,21 +6,25 @@
 // @todo
 // That's lame.
 // Move outside of template.
-$ad_image = file_create_url($content['field_ad_image']['#items'][0]['uri']);
-$content['field_ad_url']['#items'][0]['html'] = TRUE;
-$content['field_ad_url']['#items'][0]['attributes']['style'] = 'width:100%; height:90px; background: url("' . $ad_image . '") 50% 0 no-repeat; display:block;margin: 0 auto;border:none;';
 $type = field_get_items('node', $node, 'field_ad_type');
 $type = $type[0]['value'];
-$ad_url = isset($content['field_ad_url']['#items'][0]['url']) ? $content['field_ad_url']['#items'][0]['url'] : '';
-$ad_body = isset($body[0]['safe_value']) ? $body[0]['safe_value'] : '';
-?>
+$ad_content = '';
 
+if ($type == 'image' && isset($content['field_ad_image']['#items'][0]['uri'])) {
+  $ad_image = file_create_url($content['field_ad_image']['#items'][0]['uri']);
+  $content['field_ad_url']['#items'][0]['html'] = TRUE;
+  $content['field_ad_url']['#items'][0]['attributes']['style'] = 'width:100%; height:90px; background: url("' . $ad_image . '") 50% 0 no-repeat; display:block;margin: 0 auto;border:none;';
+
+  $ad_content = l('', $ad_url, $content['field_ad_url']['#items'][0]);
+}
+else {
+  $ad_url = isset($content['field_ad_url']['#items'][0]['url']) ? $content['field_ad_url']['#items'][0]['url'] : '';
+  $ad_body = isset($body[0]['safe_value']) ? $body[0]['safe_value'] : '';
+  $ad_content = $ad_body;
+}
+?>
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <div class="advertisement">
-    <?php if ($type == 'image') : ?>
-    <?php print l('', $ad_url, $content['field_ad_url']['#items'][0]); ?>
-    <?php else: ?>
-    <?php print $ad_body; ?>
-    <?php endif; ?>
+    <?php print $ad_content; ?>
   </div>
 </article>

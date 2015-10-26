d0d7b6386bb01c24439a0388a1de98590b5cc7a8
mak <mak@peytz.dk>
2015-10-26 18:05:57 +0200

    Add email styles into mimemail template. Add mailsystem configurations to feature.

--- themes/yellow_pages/templates/mimemail-message.tpl.php
+++ themes/yellow_pages/templates/mimemail-message.tpl.php
@@ -23,35 +23,38 @@ $theme_path = realpath(drupal_get_path('theme', 'yellow_pages'));
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <?php if ($css): ?>
-      <style type="text/css">
-        <?php print $css ?>
-      </style>
-    <?php endif; ?>
   </head>
-  <body id="mimemail-body" <?php if ($module && $key): print 'class="'. $module .'-'. $key .'"'; endif; ?>>
-    <div id="center">
-      <div id="main">
-        <div class="mail-header">
-          <img src="<?php print $theme_path; ?>/images/mail-header.png" />
-        </div>
-        <div class="mail-text">
-          <?php print $body ?>
-        </div>
-        <div class="mail-social" style="padding: 10px 20px;">
-          <p><?php print t('Social links:'); ?></p>
-          <p>
-            <a href="#" class="social-link google-plus" style="display: inline-block; height: 25px; width: 50px;"><img src="profiles/yp/themes/yellow_pages/images/google.png" /></a>
-            <a href="#" class="social-link facebook" style="display: inline-block; height: 25px; width: 50px;"><img src="profiles/yp/themes/yellow_pages/images/facebook2.png" /></a>
-            <a href="#" class="social-link twitter" style="display: inline-block; height: 25px; width: 50px;"><img src="profiles/yp/themes/yellow_pages/images/twitter.png" /></a>
-            <a href="#" class="social-link linkedin" style="display: inline-block; height: 25px; width: 50px;"><img src="profiles/yp/themes/yellow_pages/images/linkedin.png" /></a>
-            <a href="#" class="social-link vkontakte" style="display: inline-block; height: 25px; width: 50px;"><img src="profiles/yp/themes/yellow_pages/images/vkontakte.png" /></a>
-          </p>
-        </div>
-        <div class="mail-footer" style="background-color: #2e2e2e; padding: 10px 20px;">
-          <p><?php print t('123 Stefan cel Mare str., Chisinau, Republic of Moldova'); ?></p>
-          <p>2015 &copy; Yellow Pages of Moldova</p>
-        </div>
+  <body style="text-align: center;">
+    <div style="display: inline-block; width: 600px;">
+      <div style="display: block; height: 424px; width: 600px;">
+        <img src="<?php print $theme_path; ?>/images/mail-header.png" />
+      </div>
+      <div style="background-color: #f4f4f4; margin-top: -190px; padding: 10px 20px; text-align: left; width: 600px; color: #2e2e2e; box-sizing: border-box;">
+        <?php print $body ?>
+      </div>
+      <div style="padding: 10px 20px; color: #2e2e2e; box-sizing: border-box;">
+        <p><?php print t('Social links:'); ?></p>
+        <p>
+          <a href="#" style="display: inline-block; height: 25px; width: 50px; text-decoration: none;">
+            <img src="<?php print $theme_path; ?>/images/google.png" />
+          </a>
+          <a href="#" style="display: inline-block; height: 25px; width: 50px; text-decoration: none;">
+            <img src="<?php print $theme_path; ?>/images/facebook2.png" />
+          </a>
+          <a href="#" style="display: inline-block; height: 25px; width: 50px; text-decoration: none;">
+            <img src="<?php print $theme_path; ?>/images/twitter.png" />
+          </a>
+          <a href="#" style="display: inline-block; height: 25px; width: 50px; text-decoration: none;">
+            <img src="<?php print $theme_path; ?>/images/linkedin.png" />
+          </a>
+          <a href="#" style="display: inline-block; height: 25px; width: 50px; text-decoration: none;">
+            <img src="<?php print $theme_path; ?>/images/vkontakte.png" />
+          </a>
+        </p>
+      </div>
+      <div class="mail-footer" style="background-color: #2e2e2e; padding: 10px 20px; color: white; box-sizing: border-box;">
+        <p><?php print t('123 Stefan cel Mare str., Chisinau, Republic of Moldova'); ?></p>
+        <p>2015 &copy; Yellow Pages of Moldova</p>
       </div>
     </div>
   </body>

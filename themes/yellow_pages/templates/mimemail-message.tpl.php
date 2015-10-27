016494163012c4a6ac9e3fda7aeb60f26b41743d
mak <mak@peytz.dk>
2015-10-27 11:43:31 +0200

    Add translations to Yellow Pages theme. Fix Gmail background image.

--- themes/yellow_pages/templates/mimemail-message.tpl.php
+++ themes/yellow_pages/templates/mimemail-message.tpl.php
@@ -26,13 +26,13 @@ $theme_path = realpath(drupal_get_path('theme', 'yellow_pages'));
   </head>
   <body style="text-align: center;">
     <div style="display: inline-block; width: 600px;">
-      <div style="display: block; height: 424px; width: 600px;">
+      <div style="display: block; max-height: 234px; width: 600px;">
         <img src="<?php print $theme_path; ?>/images/mail-header.png" />
       </div>
-      <div style="background-color: #f4f4f4; margin-top: -190px; padding: 10px 20px; text-align: left; width: 600px; color: #2e2e2e; box-sizing: border-box;">
+      <div style="background-color: #f4f4f4; padding: 10px 20px; text-align: left; width: 560px; color: #2e2e2e;">
         <?php print $body ?>
       </div>
-      <div style="padding: 10px 20px; color: #2e2e2e; box-sizing: border-box;">
+      <div style="padding: 10px 20px; color: #2e2e2e; width: 560px;">
         <p><?php print t('Social links:'); ?></p>
         <p>
           <a href="#" style="display: inline-block; height: 25px; width: 50px; text-decoration: none;">

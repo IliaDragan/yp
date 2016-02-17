3e164987dca0dda7f87bce0a9f5487ebe70238d3
mak <mak@peytz.dk>
2016-02-18 01:07:00 +0200

    Add ads node view count table. Create code for count ads views -- not finished.

---
+++ modules/yellow_pages_statistics/js/yellow_pages_statistics.nvc.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) {
+  var yp_ads_nvc_insert_node_view = function(nids) {
+    $.ajax({
+      type: 'POST',
+      url: Drupal.settings.basePath + 'yp/ads-nvc',
+      dataType: 'json',
+      data: JSON.stringify(nids)
+    });
+  }
+
+  var yp_ads_nvc_nids =  Drupal.settings.yp_ads_nvc;
+  yp_ads_nvc_insert_node_view(yp_ads_nvc_nids);
+});
